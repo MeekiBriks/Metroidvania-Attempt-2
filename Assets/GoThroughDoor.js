82c0b0634f0dee94ee173198f5f5e235e0de70c8
Unknown <escrime.ali@virginmedia.com>
2017-07-12 21:47:48 +0100

    Added a door that you can go through with "e" ;(

---
+++ Assets/GoThroughDoor.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+import UnityEngine.SceneManagement;
+
+var playerOnDoor : PlayerOnDoor;
+var interactKey : String = "e";
+
+function Start () {
+    playerOnDoor = GetComponent.<PlayerOnDoor>();
+}
+
+function Update () {
+    if(Input.GetKeyDown(interactKey) && playerOnDoor.playerOnDoor){
+        SceneManager.LoadScene("Level02");
+    }
+}
---
+++ Assets/PlayerOnDoor.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+var playerOnDoor : System.Boolean;
+
+function OnTriggerEnter (collider : Collider) {
+    if(collider == GameObject.Find("Character Model").GetComponent.<CapsuleCollider>()){
+        playerOnDoor = true;
+    }
+}function OnTriggerExit (collider : Collider) {
+    if(collider == GameObject.Find("Character Model").GetComponent.<CapsuleCollider>()){
+        playerOnDoor = false;
+    }
+}
