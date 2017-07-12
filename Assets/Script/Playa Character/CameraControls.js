814000635b41c130aa7c3ca3956f4d82b190ae3a
Daniel <daniel.bradley24@gmail.com>
2017-07-12 23:13:26 +0100

    Character Movement and Slope Realism
    nice cool slops

---
+++ Assets/Script/Playa Character/CameraControls.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+var cameraXPosition : float;
+var cameraYPosition : float;
+
+
+function Start () {
+	
+};
+
+function Update () {
+	cameraXPosition = GameObject.Find("Character Model").transform.position.x;
+	cameraYPosition = GameObject.Find("Character Model").transform.position.y;
+	transform.position = Vector3(cameraXPosition,cameraYPosition + 3.5,-10);
+	transform.rotation = Quaternion.Euler(Vector3(0,0,0));
+};
---
+++ Assets/Script/Playa Character/Character Movement.js
@@ -0,0 +1,46 @@
+﻿#pragma strict
+
+var leftKey = "left";
+var rightKey = "right";
+var jumpKey = "space";
+var ballKey;
+var upKey;
+var downKey;
+var interactKey;
+
+var xSpeed : float;
+var jumpHeight : float;
+var slopeSteepness : float;
+var jitterHeight : float;
+
+var xMovement : Vector3;
+var yMovement : Vector3;
+
+var ballForm;
+var aimUp;
+var aimDown;
+var facingRight;
+
+
+function Start () {
+	xSpeed = 4;
+	jumpHeight = 4;
+	ballForm = false;
+	aimUp = false;
+	aimDown = false;
+	facingRight = true;
+	slopeSteepness = 60;
+	jitterHeight = 4 * Mathf.Tan(60);
+};
+
+function Update () {
+	xMovement = Vector3(Input.GetAxis("Horizontal"),0,0);
+	yMovement = Vector3(0,jitterHeight,0);
+	transform.position += yMovement * Time.deltaTime;
+	transform.position += xMovement * xSpeed * Time.deltaTime;
+	transform.position -= yMovement * Time.deltaTime;
+	if (Input.GetKey(jumpKey) && Function.On
+};
+
+function lastUpdate() {
+}
