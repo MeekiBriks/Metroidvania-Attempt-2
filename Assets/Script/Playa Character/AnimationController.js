eb5b56dce5172dacf90911ed69680d41df2085d7
Daniel <daniel.bradley24@gmail.com>
2017-07-13 04:15:31 +0100

    Added a AnimationController for the 1 total animation. Read desc.
    - Unable to fix collision checking for jump.
    - Unable to correctly implement a system for checking what direction the character should be facing in AnimationController (shouldn't become false when stationary AND facing right).
    - SUCCESSFULLY REORGANISED 2 WHOLE SCRIPTS AND SOME .memer FILES

---
+++ Assets/Script/Playa Character/AnimationController.js
@@ -0,0 +1,36 @@
+﻿#pragma strict
+
+var lastAnimationKeyPressed;
+
+var running : System.Boolean;
+var ballForm : System.Boolean;
+var aimUp : System.Boolean;
+var aimDown : System.Boolean;
+var facingRight : System.Boolean;
+
+var xMovement;
+
+function Start () {
+	running = false;
+	ballForm = false;
+	aimUp = false;
+	aimDown = false;
+	facingRight = true;
+}
+
+function Update () {
+	xMovement = gameObject.GetComponent(CharacterMovement).xMovement;
+	if (xMovement != Vector3.zero) {
+		DetermineFacingDirection();
+	};
+};
+
+function DetermineFacingDirection() {
+	switch (xMovement) {
+	case (Vector3.right):
+		facingRight = true;
+		break;
+	default:
+		facingRight = false;
+	};
+};
--- Assets/Script/Playa Character/CharacterMovement.js
+++ Assets/Script/Playa Character/CharacterMovement.js
@@ -6,7 +6,7 @@ var jumpKey = "space";
 var ballKey;
 var upKey;
 var downKey;
-var interactKey;
+var interactKey = "e";
 
 var xSpeed : float;
 var jumpHeight : float;
@@ -16,20 +16,11 @@ var jitterHeight : float;
 var xMovement : Vector3;
 var yMovement : Vector3;
 
-var ballForm;
-var aimUp;
-var aimDown;
-var facingRight;
 var onFloor : System.Boolean;
-
 
 function Start () {
 	xSpeed = 4;
 	jumpHeight = 4;
-	ballForm = false;
-	aimUp = false;
-	aimDown = false;
-	facingRight = true;
 	slopeSteepness = 60;
 	jitterHeight = 4 * Mathf.Tan(60);
 };
@@ -39,19 +30,20 @@ function Update () {
 	yMovement = Vector3(0,jitterHeight,0);
 	transform.position += yMovement * Time.deltaTime;
 	transform.position += xMovement * xSpeed * Time.deltaTime;
-	transform.position -= yMovement * Time.deltaTime;
 };
 
-function lastUpdate() {
+function LateUpdate() {
+	yMovement = Vector3(0,jitterHeight,0);
+	transform.position -= yMovement * Time.deltaTime;
 }
 
 function OnCollisionEnter (collision : Collision){
-    if(collision == GameObject.Find("Floor").GetComponent.<BoxCollider>()){
+    if(collision == gameObject.Find("Floor").GetComponent.<BoxCollider>()){
         onFloor = true;
         xSpeed = 100;
     }
 }function OnCollisionExit (collision : Collision){
-    if(collision == GameObject.Find("Floor").GetComponent.<BoxCollider>()){
+    if(collision == gameObject.Find("Floor").GetComponent.<BoxCollider>()){
         onFloor = false;
         xSpeed = 4;
     }
