992735b59615b06a754a495d2148ad232c638074
Daniel <daniel.bradley24@gmail.com>
2017-07-13 20:04:46 +0100

    Fleshed out AnimationController, read desc for CharMovement changes
    Added variables and functions to calculate the velocity of the playermodel using fixed update.
    Added a cross-script function callup to OnCollisionEnter to control animations when landing from a falling or jumping state.

--- Assets/Script/Playa Character/AnimationController.js
+++ Assets/Script/Playa Character/AnimationController.js
@@ -1,36 +1,141 @@
 ﻿#pragma strict
 
-var lastAnimationKeyPressed;
+// Temporary Input binds, change when input manager has been figured out.
+var leftKey = "left";
+var rightKey = "right";
+var jumpKey = "space";
+var ballKey = "f";
+var upKey = "up";
+var downKey = "down";
+var interactKey = "e";
+var schuutKey = "z";
 
+// Animation Control Variables
+var idleing : System.Boolean;
 var running : System.Boolean;
 var ballForm : System.Boolean;
+var idleBallForm : System.Boolean;
+var lookUp : System.Boolean;
+var lookDown : System.Boolean;
 var aimUp : System.Boolean;
 var aimDown : System.Boolean;
 var facingRight : System.Boolean;
+var jumping : System.Boolean;
+var falling : System.Boolean;
+var inFight : System.Boolean;
 
-var xMovement;
+// Animation Control Triggers
+var lastKeyPressed;
+var currentVelocity : Vector3;
 
-function Start () {
+
+function Start() {
+	idleing = true;
 	running = false;
+	jumping = false;
+	falling = false;
 	ballForm = false;
 	aimUp = false;
 	aimDown = false;
 	facingRight = true;
 }
 
-function Update () {
-	xMovement = gameObject.GetComponent(CharacterMovement).xMovement;
-	if (xMovement != Vector3.zero) {
-		DetermineFacingDirection();
-	};
-};
+function Update() {
+	DetermineKeyPress();
+	AnimationVariableChanger();
+	CheckForFall();
+}
 
-function DetermineFacingDirection() {
-	switch (xMovement) {
-	case (Vector3.right):
-		facingRight = true;
-		break;
-	default:
-		facingRight = false;
-	};
-};
+function LateUpdate() {
+	lastKeyPressed = "undefined";
+}
+
+function DetermineKeyPress() {
+	if (Vector3(Input.GetAxis("Horizontal"),0,0) == Vector3.right) {
+		lastKeyPressed = rightKey;
+	}
+	if (Vector3(Input.GetAxis("Horizontal"),0,0) == Vector3.left) {
+		lastKeyPressed = leftKey;
+	}
+	if (Input.GetKeyDown(jumpKey) === true) {
+		lastKeyPressed = jumpKey;
+	}
+	if (Input.GetKeyDown(ballKey) === true) {
+		lastKeyPressed = ballKey;
+	}
+	if (Vector3(0,Input.GetAxis("Vertical"),0) == Vector3.up) {
+		lastKeyPressed = upKey;
+	}
+	if (Vector3(0,Input.GetAxis("Vertical"),0) == Vector3.down) {
+		lastKeyPressed = downKey;
+	}
+	if (Input.GetKeyDown(interactKey) === true) {
+		lastKeyPressed = interactKey;
+	}
+AnimationVariableChanger();
+}
+
+function OnFloor() {
+	jumping = false;
+	falling = false;
+}
+
+function CheckForFall() {
+	currentVelocity = GameObject.Find("Character Model").GetComponent(CharacterMovement).currentVelocity;
+	if (currentVelocity.y <= -0.1) {
+		falling = true;
+	}
+}
+
+function AnimationVariableChanger() {
+	switch (lastKeyPressed) {
+		case rightKey:
+			facingRight=true;
+			break;
+		case leftKey:
+			facingRight=false;
+			break;
+		case jumpKey:
+			jumping=true;
+			idleing = false;
+			break;
+		case ballKey:
+			if (ballForm === false) {
+				ballForm = true;
+				idleing = false; //replace with transition function
+			}
+			if (ballForm === true) {
+				ballForm = false;
+				idleing = true; //replace with transition function
+			}
+			break;
+		case upKey:
+			if (inFight === true) {
+				aimUp=true;
+				idleing = false;
+			} else {
+				lookUp=true;
+				idleing = false;
+			}
+			break;
+		case downKey:
+			if (inFight === true) {
+				aimDown=true;
+				idleing = false;
+			} else {
+				lookDown=true;
+				idleing = false;
+			}
+			break;
+		case interactKey:
+			facingRight=true;
+			idleing = false;
+			break;
+		default:
+			idleing = true;
+			aimUp=false;
+			lookUp=false;
+			aimDown=false;
+			lookDown=false;
+	}
+}
--- Assets/Script/Playa Character/CharacterMovement.js
+++ Assets/Script/Playa Character/CharacterMovement.js
@@ -1,5 +1,6 @@
 ﻿#pragma strict
 
+// Temporary Input binds, change when input manager has been figured out.
 var leftKey = "left";
 var rightKey = "right";
 var jumpKey = "space";
@@ -8,43 +9,69 @@ var upKey;
 var downKey;
 var interactKey = "e";
 
+// Movement Startup variables
 var xSpeed : float;
 var jumpHeight : float;
 var slopeSteepness : float;
 var jitterHeight : float;
 
+//Movement intermediatry and Output variables
 var xMovement : Vector3;
 var yMovement : Vector3;
-
 var onFloor : System.Boolean;
+
+//Calculate velocity
+var oldPosition : Vector3;
+var newPosition : Vector3;
+var currentVelocity : Vector3;
+
 
 function Start () {
 	xSpeed = 4;
 	jumpHeight = 4;
 	slopeSteepness = 60;
 	jitterHeight = 4 * Mathf.Tan(60);
-};
+	DirectionVector();
+	PerSecondTimer();
+}
 
 function Update () {
 	xMovement = Vector3(Input.GetAxis("Horizontal"),0,0);
 	yMovement = Vector3(0,jitterHeight,0);
 	transform.position += yMovement * Time.deltaTime;
 	transform.position += xMovement * xSpeed * Time.deltaTime;
-};
+}
+
+function FixedUpdate() {
+	DirectionVector();
+}
 
 function LateUpdate() {
 	yMovement = Vector3(0,jitterHeight,0);
 	transform.position -= yMovement * Time.deltaTime;
 }
 
+function PerSecondTimer() {
+	yield WaitForSeconds (1);
+}
+	
+
 function OnCollisionEnter (collision : Collision){
-    if(collision == gameObject.Find("Floor").GetComponent.<BoxCollider>()){
-        onFloor = true;
-        xSpeed = 100;
-    }
+	if(collision == gameObject.Find("Floor").GetComponent.<BoxCollider>()){
+		onFloor = true;
+		xSpeed = 100;
+		GameObject.Find("Character Model").GetComponent(AnimationController).OnFloor();
+	}
 }function OnCollisionExit (collision : Collision){
-    if(collision == gameObject.Find("Floor").GetComponent.<BoxCollider>()){
-        onFloor = false;
-        xSpeed = 4;
-    }
+	if(collision == gameObject.Find("Floor").GetComponent.<BoxCollider>()){
+		onFloor = false;
+		xSpeed = 4;
+	}
+}
+
+function DirectionVector() {
+	oldPosition = transform.position;
+	yield PerSecondTimer();
+	newPosition = transform.position;
+	currentVelocity = newPosition - oldPosition;
 }
